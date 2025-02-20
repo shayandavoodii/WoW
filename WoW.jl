d2d51f4b2feb56272c9f05ef32f9089d3332e603
Shayan <sh0davoodi@gmail.com>
2025-02-21 01:15:56 +0330

    Minot changes

--- WoW.jl
+++ WoW.jl
@@ -1,7 +1,25 @@
 using HTTP, JSON3
 
-remote_repo_url = "https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sing-box.json"
-output_file_path = "modified_file.json"
+const remote_repo_url = "https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sing-box.json"
+const output_file_path = "modified_file.json"
+
+function convert_json3_to_dict(json3_data)
+  if json3_data isa JSON3.Object
+      dict_data = Dict{String, Any}()
+      for (key, value) in json3_data
+          dict_data[string(key)] = convert_json3_to_dict(value)
+      end
+      return dict_data
+  elseif json3_data isa JSON3.Array
+      array_data = []
+      for item in json3_data
+          push!(array_data, convert_json3_to_dict(item))
+      end
+      return array_data
+  else
+      return json3_data
+  end
+end
 
 response = HTTP.get(remote_repo_url)
 json_string = String(response.body)
@@ -18,22 +36,4 @@ json_data["route"]["rules"][3]["strategy"] = "prefer_ipv6"
 # Use JSON3.pretty to format the JSON for readability
 open(output_file_path, "w") do io
     JSON3.pretty(io, json_data)
-end
-
-function convert_json3_to_dict(json3_data)
-    if json3_data isa JSON3.Object
-        dict_data = Dict{String, Any}()
-        for (key, value) in json3_data
-            dict_data[string(key)] = convert_json3_to_dict(value)
-        end
-        return dict_data
-    elseif json3_data isa JSON3.Array
-        array_data = []
-        for item in json3_data
-            push!(array_data, convert_json3_to_dict(item))
-        end
-        return array_data
-    else
-        return json3_data
-    end
 end
